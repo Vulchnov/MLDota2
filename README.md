c9067c3b9340428fda0b372554b6051bac57241b
Vulchnov <fr0st3e5@gmail.com>
2024-05-09 18:56:14 -0600

    Final commit

---
+++ README.md
@@ -0,0 +1,10 @@
+To run the code go into the Code folder and run either
+"python3 KNN3.py"
+"python3 KNN5.py"
+"python3 KNN10.py"
+Depending on which value of K you want, that will create a txt file in that folder with the results of the predictions
+
+If you move those txt files back to the main file and run "python3 results.py" 
+The results of each of the knn will be printed out. 
+
+The code is well optimized and the three of them can all be ran in parallel without problems, all three should complete in just over a day for the full 10,000 predictions for the dota2Test.csv
--- results.py
+++ results.py
@@ -1,10 +1,37 @@
 
-with open("k5.txt", "r") as file:
+with open("k3.txt", "r") as file:
     right = 0
-    for i in range(1000):
-        read = file.readline()
+    total = 0
+    read = file.readline()
+    while read != "":
         if read == "1\n":
             right += 1
+        total += 1
+        read = file.readline()
 
-print(f"{right/1000}% Correct")
+print(f"K = 3: {right/total}% Correct")
         
+
+with open("k5.txt", "r") as file:
+    right = 0
+    total = 0
+    read = file.readline()
+    while read != "":
+        if read == "1\n":
+            right += 1
+        total += 1
+        read = file.readline()
+
+print(f"K = 5: {right/total}% Correct")
+
+with open("k10.txt", "r") as file:
+    right = 0
+    total = 0
+    read = file.readline()
+    while read != "":
+        if read == "1\n":
+            right += 1
+        total += 1
+        read = file.readline()
+
+print(f"K = 10: {right/total}% Correct")
